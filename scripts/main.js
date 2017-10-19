aabcf30be93d83b6e12ca6db79f2cf8e8ffdaea4
Joe Davis <joethedave@gmail.com>
2017-10-18 20:29:55 -0500

    fixed a couple of bugs

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ var alive = true;
 var dead = false;
 var active = false;
 var runGame = false;
-var filler = false;
+var filler = dead;
 var gameBoard = [];
 var iteration = 0;
 var iterationSpeed = 100;
@@ -52,49 +52,38 @@ function CreateGameBoard() {
     }
 }
 
+function applyFiller(element) {
+    if (filler && $(element).hasClass('alive')) {
+        $(element).removeClass('alive');
+    }
+    else if (!filler && !$(element).hasClass('alive')) {
+        $(element).addClass('alive');
+    }
+}
+
 function DrawGameBoard() {
     for (var x = 0; x < cellCount; x++) {
         $('.main').append('<div id="cell-' + gameBoard[x].id + '" class="cell" style="width: ' + (960/xAxis) + 'px; height: ' + (960/xAxis) + 'px;"></div>');
         gameBoard[x].$element = $('#cell-' + gameBoard[x].id);
-        gameBoard[x].$element.hover(
-            function (e) {  if (!active)    {   $(this).addClass('hovered');    }   },
-            function (e) {  if (!active)    {  $(this).removeClass('hovered');  }   }
-        );
-        gameBoard[x].$element.mousedown (       // I would guess the bug with only every other column starting the fill is in lines 63-97.
-            function (e) {                      // But I can't for the life of me figure out what it is that would target only half of the cells...
-                if (!active) {
-                    if ($(this).hasClass('alive'))
-                        {   filler = alive; }
-                    else
-                        {   filler = dead;  }
-                  $('.cell').bind(
-                      'mouseover', function (e){
-                        if (filler && $(this).hasClass('alive')) {
-                            $(this).removeClass('alive');
-                        }
-                        else if (!filler && !$(this).hasClass('alive')) {
-                            $(this).addClass('alive');
-                        }
-                      }
-                  );
-              }
-          }
-        )
-      .mouseup(
-            function (e) {
-        $('.cell').unbind('mouseover');
+        gameBoard[x].$element.hover(function (e) {
+            if (!active) {
+                $(this).addClass('hovered');
+                if (e.which === 1) {
+                    applyFiller(this);
+                }
             }
-        );
-      $('.cell').mousedown(
-          function (e) {
+        }, function (e) {
+            if (!active) {
+                $(this).removeClass('hovered');  
+            }
+        }).on('mousedown', function() {
             if ($(this).hasClass('alive')) {
-                $(this).removeClass('alive');
+                filler = alive;
+            } else {
+                filler = dead;
             }
-            else if (!$(this).hasClass('alive')) {
-                $(this).addClass('alive');
-            }   
-          }
-      );
+            applyFiller(this);
+        });
     }
     $('.main').append('<div class="clear"></div>');
 }
@@ -121,13 +110,11 @@ function SwitchIterating()   {
         runGame = setInterval(IterateGameBoard, iterationSpeed);
     }
     
-    if ($('switch-iteration').hasClass('iterating')) {  // I'm having trouble getting this chunk to work; all it should do is switch the text of the button.
-        $('switch-iteration').removeClass('iterating'); // I'm not sure of the syntax I need, though, and I've fidgeted with a dozen or so different methods.
-        $('switch-iteration').val('START ITERATING');
+    if (!active) {  
+        $('#switch-iteration').text('START ITERATING');
     }
     else {
-        $('switch-iteration').addClass('iterating');
-        $('switch-iteration').attr('value', 'STOP ITERATING');
+        $('#switch-iteration').text('STOP ITERATING');
     }
 }
 
